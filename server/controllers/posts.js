9791fa8ee7967c29dcad74e3bbbfd085d591fcbe
egladoun <egladoun@outlook.com>
2021-11-11 14:18:15 -0500

    updated again 11/11

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,126 +1,54 @@
-//* B"H
+/*  B"H
 */
-const Users = require( "./users");
-const { ObjectId } = require('bson');
-const { client } = require('./mongo');
+const express = require("express");
+const model = require("../models/posts");
 
-const collection = client.db(process.env.MONGO_DB).collection('posts');
-module.exports.collection = collection;
+const app = express.Router();
 
-const list = [
-    { 
-        src: "https://bulma.io/images/placeholders/1280x960.png",
-        alt: "Placeholder image",
-        caption: "Lorem Ipsom",
-        time: Date(),
-        user_handle: "@johnsmith",
-        isPublic: true,
-    },
-    { 
-        src: "https://bulma.io/images/placeholders/1280x960.png",
-        alt: "Placeholder image",
-        caption: "We want Moshiach Now",
-        time: Date(),
-        user_handle: "@vp",
-        isPublic: true,
-    },
-    { 
-        src: "https://scontent-lga3-2.xx.fbcdn.net/v/t31.18172-8/p720x720/886090_10100137903372610_773365632_o.jpg?_nc_cat=101&ccb=1-5&_nc_sid=ba80b0&_nc_ohc=DWVdkKM2PwQAX_tAc5p&_nc_ht=scontent-lga3-2.xx&oh=d9d284fef84a57b1a824932d5fd2da20&oe=618A0203",
-        alt: "Purim in SUB #100",
-        caption: "What a purim to remember",
-        time: Date(),
-        user_handle: "@JewPaltz",
-        isPublic: true,
-    },
-    { 
-        src: "https://scontent-lga3-2.xx.fbcdn.net/v/t1.6435-9/s600x600/244029201_10167312248050347_4050463819121596219_n.jpg?_nc_cat=101&ccb=1-5&_nc_sid=8bfeb9&_nc_ohc=oaf-csovSFEAX-I2hxQ&_nc_ht=scontent-lga3-2.xx&oh=8466fcd68032477fab99306ba1a6e800&oe=61895C64",
-        alt: "Mug with slogan",
-        caption: "Never be afraid to try something new. The ark was built by amateurs and the Titanic by professionals",
-        time: Date(),
-        user_handle: "@JewPaltz",
-        isPublic: true,
-    },
-    { 
-        src: "https://scontent-lga3-2.xx.fbcdn.net/v/t1.6435-9/p180x540/242759506_10102663165018030_5506732176336636339_n.jpg?_nc_cat=104&ccb=1-5&_nc_sid=730e14&_nc_ohc=LJFWaOTJXvUAX-skiU3&_nc_ht=scontent-lga3-2.xx&oh=e6a03799ccf969c3b3fe62a7b266b8b9&oe=618C38F8",
-        alt: "The family",
-        caption: "The whole family. All the kids hiking together. Sukkot Vacation.",
-        time: Date(),
-        user_handle: "@JewPaltz",
-        isPublic: true,
-    },
-];
-
-const addOwnerPipeline = [
-    {"$lookup" : {
-        from: "users",
-        localField: 'user_handle',
-        foreignField: 'handle',
-        as: 'owner',
-    }},
-    {$unwind: "$owner"},
-    { $project: { "owner.password": 0}}
-];
-
-module.exports.GetAll = function GetAll() {
-    return collection.aggregate(addOwnerPipeline).toArray();
-}
-
-module.exports.GetWall = function GetWall(handle) {
-    return collection.aggregate(addOwnerPipeline).match({ user_handle: handle }).toArray();
-}
-
-// TODO: convert to MongoDB
-module.exports.GetFeed = function GetFeed(handle) {
-    const query = Users.collection.aggregate([
-        {$match: { handle }},
-        {"$lookup" : {
-            from: "posts",
-            localField: 'following.handle',
-            foreignField: 'user_handle',
-            as: 'posts'
-        }},
-        {$unwind: '$posts'},
-        {$replaceRoot: { newRoot: "$posts" } },
-    ].concat(addOwnerPipeline));
-    return query.toArray();
-    //return listWithOwner()
-    //.match(post=> GetByHandle(handle).following.some(f=> f.handle == post.user_handle && f.isApproved) );
-}
-
-
-module.exports.Get = function Get(post_id) { return collection.findOne({_id: new ObjectId(post_id) }); }
-
-module.exports.Add = async function Add(post) {
-    if(!post.user_handle){
-        throw {code: 422, msg: "Post must have an Owner"}
-    }
-    post.time = Date();
-    
-    const response = await collection.insertOne(post);
-    
-    post.id = response.insertedId;
-
-    return { ...post };
-}
-module.exports.Update = async function Update(post_id, post) {
-    const results = await collection.findOneAndUpdate(
-        {_id: new ObjectId(post_id) }, 
-        { $set: post },
-        { returnDocument: 'after'}
-    );
-
-    return results.value;
-}
-module.exports.Delete = async function Delete(post_id) {
-    const results = await collection.findOneAndDelete({_id: new ObjectId(post_id) })
-
-    return results.value;
-} 
-
-module.exports.Search = q => collection.find({ caption: new RegExp(q,"i") }).toArray();
-
-module.exports.Seed = async ()=>{
-    for (const x of list) {
-        await this.Add(x)
-    }
-}
+app
+    .get("/", (req, res, next) =>{
+        model   .GetAll()
+                .then( x=> res.send(x) )
+                .catch(next) 
+    })
+    .get("/wall/:handle", (req, res, next) =>{
+        model   .GetWall(req.params.handle)
+                .then( x=> res.send(x) )
+                .catch(next)    
+    })
+    .get("/feed/:handle", (req, res, next) =>{
+        model   .GetFeed(req.params.handle)
+                .then( x=> res.send(x) )
+                .catch(next)    
+    })
+    .get("/search", (req, res, next) =>{
+        model   .Search(req.query.q)
+                .then( x=> res.send(x) )
+                .catch(next)    
+    })
+    .get("/:id", (req, res, next) =>{
+        model   .Get(req.params.id)
+                .then( x=> res.send(x) )
+                .catch(next)    
+    })
+    .post("/", (req, res, next) =>{
+        model   .Add(req.body)
+                .then( x=> res.status(201).send(x) )
+                .catch(next)
+    })
+    .patch("/:id", (req, res, next) =>{
+        model   .Update(req.params.id, req.body)
+                .then( x=> res.send(x) )
+                .catch(next) 
+    })
+    .delete("/:id", (req, res, next) =>{
+        model   .Delete(req.params.id)
+                .then( x=> res.send({ deleted: x }) )
+                .catch(next) 
+    })
+    .post("/seed", (req, res, next) =>{
+        model   .Seed()
+                .then( x=> res.status(201).send("Created") )
+                .catch(next)
+    })
+module.exports = app
